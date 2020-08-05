6088571ce1eb5b53df8df2d27c6a9c627f6fdf7f
leka-ren <polafort@gmail.com>
2020-08-05 20:53:56 +0300

    реализовано логгирование

--- app.js
+++ app.js
@@ -1,19 +1,73 @@
 require('dotenv').config();
 
-// const express = require('express');
+const express = require('express');
 
-// const { json } = require('body-parser');
+const { json } = require('body-parser');
 
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const { celebrate, Joi, errors } = require('celebrate');
+const { celebrate, Joi, errors } = require('celebrate');
 
-// const { login, createUser } = require('./controllers/users');
+const { login, createUser } = require('./controllers/users');
 
-// const app = express();
+const app = express();
 
-// const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-// const { requestLogger, errorLogger } = require('./middlewares/logger');
+const auth = require('./middlewares/auth');
 
-// const { PORT = 3042 } = process.env;
+const { PORT = 3042 } = process.env;
+
+const baseUrl = 'mongodb://localhost:27017/newsexpdb';
+
+app.use(json());
+
+mongoose.connect(baseUrl, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+app.use(requestLogger);
+
+app.post(
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required().min(8),
+    }),
+  }),
+  login,
+);
+
+app.post(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required().min(2).max(30),
+      email: Joi.string().required().email(),
+      password: Joi.string().required().min(8),
+    }),
+  }),
+  createUser,
+);
+
+app.use(auth);
+
+app.use(errorLogger);
+
+app.use(errors());
+
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res.status(statusCode).send({
+    message: statusCode === 500 ? 'Somthing wrong on server' : message,
+  });
+});
+
+// eslint-disable-next-line no-console
+app.listen(PORT, () => console.log(`Server started, listening on ${PORT}`));
